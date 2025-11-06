58f35c22c4ced6040aa62aa11b7cc0e04587c738
mufid1012 <mufidghibran1012@gmail.com>
2025-11-06 16:30:16 +0700

    menambahkan TopAppBar pada Scaffold dengan judul 'Home'

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -7,5 +7,21 @@ fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
     onSubmitClick: (String, String, String) -> Unit
 ){
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.home),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
 
 }
