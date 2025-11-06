03e4c88ef54f6be8f274e8ef8e030a2244c2bcad
mufid1012 <mufidghibran1012@gmail.com>
2025-11-06 09:27:02 +0700

    menambahkan navigasi dari FormIsian ke Detail menggunakan NavHost

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,8 +1,11 @@
 package com.example.navigasi
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import androidx.navigation.NavHost
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -15,5 +18,21 @@ fun DataApp(
     navController: NavController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
 
+            modifier = Modifier.padding(paddingValues = isiRuang)){
+                FormIsian (
+                    //pilihanJK = JenisK.map
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+
+
+
+    }
 }
