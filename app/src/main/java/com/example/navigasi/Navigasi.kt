487e4c7fdfc54109375e33ae6a32ff34d860aaf6
mufid1012 <mufidghibran1012@gmail.com>
2025-11-06 09:14:37 +0700

    menambahkan fun DataApp

--- app/src/main/java/com/example/navigasi/Navigasi.kt
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -1,6 +1,19 @@
 package com.example.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi {
     Formulir,
     Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavController = rememberNavController(),
+    modifier: Modifier
+){
+
 }
