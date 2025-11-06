e57787fe3a409db21c88a2ccaa3b66ba29a7967b
mufid1012 <mufidghibran1012@gmail.com>
2025-11-06 16:45:09 +0700

    menambahkan fun TampilData

---
+++ app/src/main/java/com/example/navigasi/view/TampilData.kt
@@ -0,0 +1,15 @@
+package com.example.navigasi.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    nama: String,
+    jenisKelamin: String,
+    alamat: String,
+    onBackBtnClick: () -> Unit
+) {
+
+}
