66fd1f782a339b5024b34ff7405b22043b27b915
mufid1012 <mufidghibran1012@gmail.com>
2025-11-06 16:28:53 +0700

    menambahkan fun FormIsian

---
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.navigasi.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmitClick: (String, String, String) -> Unit
+){
+
+}
