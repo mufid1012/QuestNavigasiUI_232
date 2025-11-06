93dbc4025efe8dd2b7256cc246c8eec276ee8447
mufid1012 <mufidghibran1012@gmail.com>
2025-11-06 16:36:31 +0700

    menambahkan divider merah

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -77,6 +77,15 @@ fun FormIsian(
                     }
                 }
             }
+
+            // ---- Divider merah----
+            HorizontalDivider(
+                modifier = Modifier
+                    .padding(all = 20.dp)
+                    .width(250.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
         }
     }
 
